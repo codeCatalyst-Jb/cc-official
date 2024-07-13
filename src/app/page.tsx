491cbc73393bbfd718655d8a5ff7fe7e17386947
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-07-13 13:16:48 +0530

    title change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "Code Catalyst",
+  description: "This is the Landing Page of code Catalyst",
   // other metadata
 };
 
